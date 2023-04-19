aada65242253e0db53daf3bd3d10d0d34729179f
Savely Makarov <125468648+lorrytruck@users.noreply.github.com>
2023-04-19 19:31:07 +0700

    Test

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World!" << endl;
+	cout << "My name is Philipp" << endl;
+	cout << "I'm 19 years old" << endl;
+	return 0;
+}
